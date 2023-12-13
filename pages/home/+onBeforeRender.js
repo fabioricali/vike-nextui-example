d129522895fe2bae39adbf6425885d7598bd2158
Fabio Ricali <f.ricali@xdevel.com>
2023-12-13 15:03:52 +0100

    remove useless code

--- pages/home/+onBeforeRender.js
+++ pages/home/+onBeforeRender.js
@@ -1,11 +1,5 @@
-import {redirect} from "vike/abort";
-
 export async function onBeforeRender(pageContext) {
-    //throw redirect('/radiotaormina')
-
     return {
-        pageContext: {
-
-        }
+        pageContext: {}
     };
 }
